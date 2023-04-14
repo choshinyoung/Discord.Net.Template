ad0fbc93fc3d5559370ff5edd41bfe31a3419b5d
choshinyoung <choshinyoung1227@gmail.com>
2023-04-15 04:11:09 +0900

    feat: IUserExtension

---
+++ Discord.Net.Template/Extensions/IUserExtension.cs
@@ -0,0 +1,26 @@
+﻿using Discord.WebSocket;
+
+namespace Discord.Net.Template.Extensions;
+
+public static class DiscordUserExtension
+{
+    public static string GetAvatar(this IUser user)
+    {
+        if (user is not SocketGuildUser guildUser)
+        {
+            return user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl();
+        }
+
+        if (guildUser.GetGuildAvatarUrl() is { } avatar)
+        {
+            return avatar;
+        }
+
+        return user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl();
+    }
+
+    public static string GetName(this IUser user)
+    {
+        return (user as SocketGuildUser)?.Nickname ?? user.Username;
+    }
+}
