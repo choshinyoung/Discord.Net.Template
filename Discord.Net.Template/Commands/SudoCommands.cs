c6787fa7328074e3ab2df20560c7c7f54d8c511d
choshinyoung <choshinyoung1227@gmail.com>
2023-04-20 01:20:00 +0900

    feat: sudo status command

--- Discord.Net.Template/Commands/SudoCommands.cs
+++ Discord.Net.Template/Commands/SudoCommands.cs
@@ -1,6 +1,7 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
 using Discord.Commands;
 using Discord.Net.Template.Extensions;
+using Discord.Net.Template.Utility;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
 
@@ -8,13 +9,13 @@ namespace Discord.Net.Template.Commands;
 
 [Group("sudo")]
 [RequireOwner]
-public partial class SudoCommands : ModuleBase<SocketCommandContext>
+public class SudoCommands : ModuleBase<SocketCommandContext>
 {
     [Command("run")]
     [Alias("eval", "execute")]
     public async Task Execute([Remainder] string code)
     {
-        var regex = CodeRegex();
+        var regex = StringUtility.CodeRegex();
 
         var match = regex.Match(code);
 
@@ -29,7 +30,7 @@ public partial class SudoCommands : ModuleBase<SocketCommandContext>
             var result = await CSharpScript.EvaluateAsync(trimmedCode,
                 ScriptOptions.Default.WithReferences(assembly).WithImports(
                     "System", "System.Collections.Generic", "System.IO", "System.Linq", "System.Net.Http",
-                    "System.Threading", "System.Threading.Tasks",
+                    "System.Threading", "System.Threading.Tasks", "System.Diagnostics",
                     "System.Text", "System.Text.RegularExpressions", "System.Net", "System.Numerics",
                     "Discord", "Discord.Commands", "Discord.Interactions",
                     "Discord.WebSocket", "Discord.Rest", "Discord.Net",
@@ -51,6 +52,28 @@ public partial class SudoCommands : ModuleBase<SocketCommandContext>
         }
     }
 
-    [GeneratedRegex("^\\s*(```(cs)?\\s*(?<block_code>.+)\\s*```)|(?<code>.+)\\s*$")]
-    private static partial Regex CodeRegex();
+    [Command("status")]
+    [Alias("info")]
+    public async Task Status()
+    {
+        var process = Process.GetCurrentProcess();
+
+        var embed = EmbedUtility.CreateEmbedFromContext(Context, title: $"{Bot.Client.CurrentUser.Username} Status");
+
+        embed.AddField("Version", Config.Get("VERSION"), true);
+        embed.AddField("Uptime", $"<t:{((DateTimeOffset)process.StartTime).ToUnixTimeSeconds()}:R>", true);
+        embed.AddEmptyField();
+
+        embed.AddField("Memory Usage", $"{process.WorkingSet64 * 1e-6:##.##}MB", true);
+        embed.AddField("Latency", $"{Bot.Client.Latency}ms", true);
+
+        embed.AddField("Servers", $"{Bot.Client.Guilds.Count} servers");
+
+        await Context.ReplyEmbedAsync(embed.Build());
+    }
+
+    [Command("restart")]
+    public async Task Restart()
+    {
+    }
 }
---
+++ Discord.Net.Template/Utility/StringUtility.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Discord.Net.Template.Utility;
+
+public static partial class StringUtility
+{
+    [GeneratedRegex("^\\s*(```(cs)?\\s*(?<block_code>.+)\\s*```)|(?<code>.+)\\s*$")]
+    public static partial Regex CodeRegex();
+}
